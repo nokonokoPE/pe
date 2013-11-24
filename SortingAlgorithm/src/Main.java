d94008e480ba4b2a8ba109aae6655663ee01c56a
nokonokoPE <Yoshiki@softbank126006125181.bbtec.net>
2013-11-24 15:39:55 +0900

    10回の平均をとるように変更

--- SortingAlgorithm/src/Main.java
+++ SortingAlgorithm/src/Main.java
@@ -9,17 +9,21 @@ public class Main {
     		datas[i] = (int)(Math.random()*DATA_SIZE);
     	}
     	
-    	long start = System.currentTimeMillis();
-    	datas = Sort.insertionSort(datas);
-    	//datas = Sort.bogoSort(datas);
-    	//datas = Sort.bubbleSort(datas);
-    	//datas = Sort.mergeSort(datas);
-    	//datas = Sort.quickSort(datas, 0, datas.length-1);
-    	long end = System.currentTimeMillis();
+    	long totalTime = 0;
+    	for(int i=0; i<10; i++) {
+	    	long start = System.currentTimeMillis();
+	    	datas = Sort.insertionSort(datas);
+	    	//datas = Sort.bogoSort(datas);
+	    	//datas = Sort.bubbleSort(datas);
+	    	//datas = Sort.mergeSort(datas);
+	    	//datas = Sort.quickSort(datas, 0, datas.length-1);
+	    	long end = System.currentTimeMillis();
+	    	totalTime = end - start;
+    	}
     	
 //    	for(int i=0; i<DATA_SIZE; i++) {
 //    		System.out.println(datas[i]);
 //    	}
-    	System.out.println(end-start);
+    	System.out.println(totalTime/10);
     }
 }
